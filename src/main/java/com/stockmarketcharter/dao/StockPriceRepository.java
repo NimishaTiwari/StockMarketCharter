2ecd64965c8f6b199e50b39f1899f8e5069ce954
Nimisha Tiwari <nimishatiwari017@gmail.com>
2021-08-06 02:31:03 +0530

    Delete src directory

--- src/main/java/com/stockmarketcharter/dao/StockPriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stockmarketcharter.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.stockmarketcharter.model.StockPriceEntity;
-
-public interface StockPriceRepository extends JpaRepository<StockPriceEntity, Integer> {
-
-}
--- src/main/java/com/stockmarketcharter/services/CompanyService.java
+++
@@ -1,8 +0,0 @@
-package com.stockmarketcharter.services;
-
-import com.stockmarketcharter.model.CompanyEntity;
-
-public interface CompanyService {
-	public CompanyEntity createCompany(CompanyEntity company,String sectorname);
-	public CompanyEntity updatecompany(CompanyEntity companyEntity, CompanyEntity company, String sectorname);
-}
